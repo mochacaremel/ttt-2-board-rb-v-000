bcf418a939416814f1ec879d920848560b92d1ea
Asheley <asheleymcdonald@gmail.com>
2018-03-15 20:49:40 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,5 @@
 # Define the variable board below.
-board = "["1", "2", "3", "4", "5", "6", "7", "8", "9"]"
+board = ["1,2,3,4,5,6,7,8,9"]
 row = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
 
 # Position 1: Left
